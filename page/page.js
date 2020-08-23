d736200da81ccc55097510c2f8753ca2762ac6d3
eli3avetta <eli3avetta@mail.ru>
2020-08-23 22:32:31 +0300

    finish calc - 1bag

--- page/page.js
+++ page/page.js
@@ -1,31 +1,31 @@
 class Calculator {
-  valueWrapperElem;
+  boardValueWrapperElem;
   btnsKeyboard;
-  operatinStrElem;
+  operatingStrElem;
   btnResult;
   resultElem;
   btnClear;
   toggleOnOff;
+  historyOperation;
   includedCalc = false;
   arithmeticSigns = ['+', '-', '*', '/', '.', '(', ')', '√', '²'];
-
   enterFirstKey = false;
-  canEnterKey = false;
-  lastKey;
+  lastNameKey;
 
   constructor() {
-    this.valueWrapperElem = document.querySelector('#value-wrapper');
+    this.boardValueWrapperElem = document.querySelectorAll('.board-value-wrapper');
     this.btnsKeyboard = document.querySelectorAll('.btn');
-    this.operatinStrElem = document.querySelector('#operating-str');
+    this.operatingStrElem = document.querySelector('#operating-str');
     this.btnResult = document.querySelector('#btn-result');
     this.resultElem = document.querySelector('#result');
     this.btnClear = document.querySelector('#clear');
     this.toggleOnOff = document.querySelector('#toggle-on-off');
+    this.historyOperation = document.querySelector('#history-operation');
 
     this.btnsKeyboard.forEach(elem => {
       elem.onclick = () => {
         const nameKey = elem.getAttribute('data-val');
-        this.showHistory(nameKey);
+        this.showOperatingStr(nameKey);
       }
     })
 
@@ -66,49 +66,44 @@ class Calculator {
         default:
           if (this.includedCalc) {
             if (Number(nameKey) || nameKey === '0' || this.arithmeticSigns.some((sign) => nameKey === sign)) {
-              this.showHistory(nameKey);
+              this.showOperatingStr(nameKey);
             }
           }
       }
     }
   }
 
-  showHistory(nameKey) {
+  showOperatingStr(nameKey) {
     if (this.enterFirstKey) {
-      if ((Number(this.lastKey) || this.lastKey === '0') && nameKey !== '√' && nameKey !== '(') {
-        this.operatinStrElem.innerHTML += nameKey;
-        this.lastKey = nameKey;
+      if ((Number(this.lastNameKey) || this.lastNameKey === '0') && nameKey !== '√' && nameKey !== '(' ||
+        this.lastNameKey === '(' && (Number(nameKey) || nameKey === '-' || nameKey === '√') ||
+        this.lastNameKey === '-' && (Number(nameKey) || nameKey === '(' || nameKey === '√') ||
+        this.lastNameKey === '²' && (Number(nameKey) || nameKey === '+' || nameKey === '-' || nameKey === '*' || nameKey === '/') ||
+        this.lastNameKey === ',' && Number(nameKey) ||
+        this.lastNameKey === '+' && nameKey === '√' ||
+        this.lastNameKey === '-' && nameKey === '√' ||
+        this.lastNameKey === '*' && nameKey === '√' ||
+        this.lastNameKey === '/' && nameKey === '√') {
+        this.operatingStrElem.innerHTML += nameKey;
+        this.lastNameKey = nameKey;
       }
       this.arithmeticSigns.forEach(sign => {
-        if (this.lastKey === sign && Number(nameKey) ||
-          this.lastKey === sign && nameKey === '(' && this.lastKey !== '(' && this.lastKey !== '²' ||
-          this.lastKey === ')' && nameKey === sign && nameKey !== ')' && nameKey !== '√'){
-          this.operatinStrElem.innerHTML += nameKey;
-          this.lastKey = nameKey;
+        if (this.lastNameKey === sign && Number(nameKey) ||
+          this.lastNameKey === sign && nameKey === '(' && this.lastNameKey !== '(' && this.lastNameKey !== '²' ||
+          this.lastNameKey === ')' && nameKey === sign && nameKey !== ')' && nameKey !== '√') {
+          this.operatingStrElem.innerHTML += nameKey;
+          this.lastNameKey = nameKey;
         }
       })
-      if (this.lastKey === '(' && (Number(nameKey) || nameKey === '-' || nameKey === '√') ||
-        this.lastKey === '-' && (Number(nameKey) || nameKey === '(' || nameKey === '√') ||
-        this.lastKey === '²' && (Number(nameKey) || nameKey === '+' || nameKey === '-' || nameKey === '*' || nameKey === '/') ||
-        this.lastKey === ',' && Number(nameKey) ||
-        this.lastKey === '+' && nameKey === '√' ||
-        this.lastKey === '-' && nameKey === '√' ||
-        this.lastKey === '*' && nameKey === '√' ||
-        this.lastKey === '/' && nameKey === '√') {
-        this.operatinStrElem.innerHTML += nameKey;
-        this.lastKey = nameKey;
-      }
-    }
-
-    if (!this.enterFirstKey && (Number(nameKey) || nameKey === '(' || nameKey === '-' || nameKey === '√')) {
-      this.operatinStrElem.innerHTML += nameKey;
-      this.lastKey = nameKey;
+    } else if (Number(nameKey) || nameKey === '(' || nameKey === '-' || nameKey === '√') {
+      this.operatingStrElem.innerHTML += nameKey;
+      this.lastNameKey = nameKey;
       this.enterFirstKey = true;
     }
   }
 
   showResult() {
-    let arithmeticStr = this.operatinStrElem.innerHTML;
+    let arithmeticStr = this.operatingStrElem.innerHTML;
     if (arithmeticStr) {
       for (let i = 0; i < arithmeticStr.length; i++) {
         let calculatedNum;
@@ -116,6 +111,7 @@ class Calculator {
         let substr;
         if (arithmeticStr[i] === '√') {
           calculatedNum = this.getRadicand(arithmeticStr)
+          console.log(calculatedNum)
           substr = arithmeticStr.substr(i, calculatedNum.length + 1);
           resultCalculatedNum = Math.sqrt(calculatedNum);
         }
@@ -129,15 +125,21 @@ class Calculator {
     }
 
     this.resultElem.innerHTML = window.eval(arithmeticStr);
+    this.showHistoryOperation(this.operatingStrElem.innerHTML, this.resultElem.innerHTML);
+  }
+
+  showHistoryOperation(operatingStr, result) {
+    this.historyOperation.innerHTML += `${ operatingStr } = <br>`;
+    this.historyOperation.innerHTML += `${ result }<br>`;
   }
 
   clearLastSymbolHistory() {
-    let historyStr = this.operatinStrElem.innerHTML;
-    this.operatinStrElem.innerHTML = historyStr.substr(0, historyStr.length - 1);
-    this.lastKey = historyStr.substr(historyStr.length - 1, 1);
+    let historyStr = this.operatingStrElem.innerHTML;
+    this.operatingStrElem.innerHTML = historyStr.substr(0, historyStr.length - 1);
+    this.lastNameKey = historyStr.substr(historyStr.length - 1, 1);
     this.arithmeticSigns.forEach(sign => {
-      if (this.lastKey === sign) {
-        this.lastKey = historyStr.substr(historyStr.length - 2, 1);
+      if (this.lastNameKey === sign) {
+        this.lastNameKey = historyStr.substr(historyStr.length - 2, 1);
       }
     });
     if (historyStr.length === 1) {
@@ -146,7 +148,7 @@ class Calculator {
   }
 
   clearAllCalculations() {
-    this.operatinStrElem.innerHTML = '';
+    this.operatingStrElem.innerHTML = '';
     this.resultElem.innerHTML = '';
     this.enterFirstKey = false;
   }
@@ -154,15 +156,16 @@ class Calculator {
   toggleCalc() {
     if (this.includedCalc) {
       this.resultElem.innerHTML = '';
-      this.operatinStrElem.innerHTML = '';
-      this.valueWrapperElem.classList.remove('mod-active');
+      this.operatingStrElem.innerHTML = '';
+      this.historyOperation.innerHTML = '';
+      this.boardValueWrapperElem.forEach(elem => elem.classList.remove('mod-active'));
       this.btnsKeyboard.forEach(elem => {
         elem.classList.remove('mod-active');
       })
       this.includedCalc = false;
     } else {
       this.resultElem.innerHTML = '';
-      this.valueWrapperElem.classList.add('mod-active');
+      this.boardValueWrapperElem.forEach(elem => elem.classList.add('mod-active'));
       this.btnsKeyboard.forEach(elem => {
         elem.classList.add('mod-active');
       })
